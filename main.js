b8ba4b1624bf1599cb6b20ce714a233500b18b24
DaiF <julie.fiadino@gmail.com>
2023-07-23 16:23:09 +0200

    scroll: scroll using one page scroll effect

--- main.js
+++ main.js
@@ -6,14 +6,17 @@ const aboutCard = document.querySelector("#about-card");
 const projectsCard = document.querySelector("#projects-card");
 
 projectsCard.style.display = "none";
-//aboutCard.style.display = "none";
 
+// Setup zoom origin
 var house = helloCard.querySelector(".house-container");
 var houseBounds = house.getBoundingClientRect();
 
 var originX = houseBounds.x + (window.innerWidth * 5.2 / 100);
 var originY = houseBounds.y + (window.innerWidth * 5 / 100);
+var origin = `${originX}px ${originY}px`;
+cardsContainer.style.transformOrigin = origin;
 
+// Setup about page position and scale
 var aboutX = originX - aboutCard.clientWidth / 1.93;
 var aboutY = originY - aboutCard.clientHeight / 1.97;
 
@@ -22,6 +25,7 @@ var aboutScale = house.clientWidth / aboutCard.clientWidth;
 aboutCard.style.transformOrigin = "50% 50%";
 aboutCard.style.transform = `translate(${aboutX}px, ${aboutY}px) scale(${aboutScale})`;
 
+// Setup projects page position and scale
 var aboutText = aboutCard.querySelector(".about-desc");
 var textBounds = aboutText.getBoundingClientRect();
 
@@ -32,52 +36,75 @@ var projectsScale = aboutScale * 0.8;
 projectsCard.style.transformOrigin = "50% 50%";
 projectsCard.style.transform = `translate(${projectsX}px, ${projectsY}px) scale(${projectsScale})`;
 
-
-var scroll = 0;
+// Scroll event
+var index = 0;
+var scrollFlag = true;
 
 addEventListener("wheel", (e) => {
-    scroll += e.deltaY;
+    if (!scrollFlag)
+        return;
 
-    if (scroll < 0)
-        scroll = 0;
-    else if (scroll > 30000)
-        scroll = 30000;
+    if (e.deltaY > 0)
+        index += 1;
+    else
+        index -= 1;
 
-    if (scroll >= 20000) {
-        projectsCard.style.display = "block";
-        aboutCard.style.display = "none";
+    if (index < 0) {
+        index = 0;
+        return;
     }
-    else if (scroll >= 14000) {
+    else if (index > 2) {
+        index = 2;
+        return;
+    }
+
+    if (index === 2) {
+        projectsCard.style.display = "block"; 
+        cardsContainer.style.transform = `scale(20) translate(0, 14px)`;
+
+        scrollFlag = false;
+        setTimeout(() => {
+            aboutCard.style.display = "none";
+            scrollFlag = true;
+        }, 1500);
+    }
+    else if (index === 1) {
         projectsCard.style.display = "block";
         aboutCard.style.display = "grid";
-        helloCard.style.display = "none";
+
+        var transform = `scale(15)`;
+        cardsContainer.style.transform = transform;
+
+        var dinoHello = helloCard.querySelector("#hello-dino");
+        dinoHello.style.transform = "scaleX(-1)";
+        dinoHello.style.left = "-1vw";
+
+        scrollFlag = false;
+        setTimeout(() => {
+            dinoHello.style.opacity = 0;
+            helloCard.style.display = "none";
+            scrollFlag = true;
+        }, 1500);
     }
     else {
         projectsCard.style.display = "none";
         helloCard.style.display = "grid";
-    }
 
-    if (scroll < 20000) {
-        var origin = `${originX}px ${originY}px`;
-        cardsContainer.style.transformOrigin = origin;
-        var transform = `scale(${1 + scroll / 1000})`
-        if (scroll >= 14000)
-            transform += `translate(0, ${(scroll - 14000) / 1000}px)`
-
-        cardsContainer.style.transform = transform;
-    }
-
-    if (scroll < 14000) {
-        // Hello card Animation
+        cardsContainer.style.transform = `scale(1)`;
+        
         var dinoHello = helloCard.querySelector("#hello-dino");
         var houseDoor = house.querySelector(".left-side");
-        dinoHello.style.left = `${-scroll / 850 + 8}vw`;
+        dinoHello.style.left = "0vw";
 
-        var dinoRight = dinoHello.getBoundingClientRect().right;
-        var houseRight = houseDoor.getBoundingClientRect().right;
-        if (houseRight < dinoRight)
+        setTimeout(() => {
             dinoHello.style.opacity = 1;
-        else
-            dinoHello.style.opacity = 0;
+            dinoHello.style.transform = "scaleX(1)";
+            dinoHello.style.left = "8vw";
+        }, 500)
+
+        scrollFlag = false;
+        setTimeout(() => {
+            scrollFlag = true;
+        }, 1500);
     }
 });
