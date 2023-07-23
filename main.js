975622809ddfcf03e5a851bffb88b6e086e0f6a2
DaiF <julie.fiadino@gmail.com>
2023-07-23 16:35:32 +0200

    chore: separate pages' scroll function

--- main.js
+++ main.js
@@ -1,45 +1,105 @@
 import './style.css'
 
+// Timeout between scroll. Time of css ease animation
+const TIMEOUT = 1500;
+
 const cardsContainer = document.querySelector("#cards-container");
 const helloCard = document.querySelector("#hello-card");
 const aboutCard = document.querySelector("#about-card");
 const projectsCard = document.querySelector("#projects-card");
 
-projectsCard.style.display = "none";
+function pageSetup() {
+    projectsCard.style.display = "none";
 
-// Setup zoom origin
-var house = helloCard.querySelector(".house-container");
-var houseBounds = house.getBoundingClientRect();
+    // Setup zoom origin
+    var house = helloCard.querySelector(".house-container");
+    var houseBounds = house.getBoundingClientRect();
 
-var originX = houseBounds.x + (window.innerWidth * 5.2 / 100);
-var originY = houseBounds.y + (window.innerWidth * 5 / 100);
-var origin = `${originX}px ${originY}px`;
-cardsContainer.style.transformOrigin = origin;
+    var originX = houseBounds.x + (window.innerWidth * 5.2 / 100);
+    var originY = houseBounds.y + (window.innerWidth * 5 / 100);
+    var origin = `${originX}px ${originY}px`;
+    cardsContainer.style.transformOrigin = origin;
 
-// Setup about page position and scale
-var aboutX = originX - aboutCard.clientWidth / 1.93;
-var aboutY = originY - aboutCard.clientHeight / 1.97;
+    // Setup about page position and scale
+    var aboutX = originX - aboutCard.clientWidth / 1.93;
+    var aboutY = originY - aboutCard.clientHeight / 1.97;
 
-var aboutScale = house.clientWidth / aboutCard.clientWidth;
+    var aboutScale = house.clientWidth / aboutCard.clientWidth;
 
-aboutCard.style.transformOrigin = "50% 50%";
-aboutCard.style.transform = `translate(${aboutX}px, ${aboutY}px) scale(${aboutScale})`;
+    aboutCard.style.transformOrigin = "50% 50%";
+    aboutCard.style.transform = `translate(${aboutX}px, ${aboutY}px) scale(${aboutScale})`;
 
-// Setup projects page position and scale
-var aboutText = aboutCard.querySelector(".about-desc");
-var textBounds = aboutText.getBoundingClientRect();
+    // Setup projects page position and scale
+    var aboutText = aboutCard.querySelector(".about-desc");
+    var textBounds = aboutText.getBoundingClientRect();
 
-var projectsX = aboutX;
-var projectsY = aboutY;
-var projectsScale = aboutScale * 0.8;
+    var projectsX = aboutX;
+    var projectsY = aboutY;
+    var projectsScale = aboutScale * 0.8;
 
-projectsCard.style.transformOrigin = "50% 50%";
-projectsCard.style.transform = `translate(${projectsX}px, ${projectsY}px) scale(${projectsScale})`;
+    projectsCard.style.transformOrigin = "50% 50%";
+    projectsCard.style.transform = `translate(${projectsX}px, ${projectsY}px) scale(${projectsScale})`;
+}
 
-// Scroll event
+// Scroll Utils
 var index = 0;
 var scrollFlag = true;
 
+function showHello() {
+    projectsCard.style.display = "none";
+    helloCard.style.display = "grid";
+
+    cardsContainer.style.transform = `scale(1)`;
+
+    var dinoHello = helloCard.querySelector("#hello-dino");
+    var house = helloCard.querySelector(".house-container");
+    var houseDoor = house.querySelector(".left-side");
+    dinoHello.style.left = "0vw";
+
+    setTimeout(() => {
+        dinoHello.style.opacity = 1;
+        dinoHello.style.transform = "scaleX(1)";
+        dinoHello.style.left = "8vw";
+    }, TIMEOUT / 3)
+
+    scrollFlag = false;
+    setTimeout(() => {
+        scrollFlag = true;
+    }, TIMEOUT);
+}
+
+function showAbout() {
+    projectsCard.style.display = "block";
+    aboutCard.style.display = "grid";
+
+    var transform = `scale(15)`;
+    cardsContainer.style.transform = transform;
+
+    var dinoHello = helloCard.querySelector("#hello-dino");
+    dinoHello.style.transform = "scaleX(-1)";
+    dinoHello.style.left = "-1vw";
+
+    scrollFlag = false;
+    setTimeout(() => {
+        dinoHello.style.opacity = 0;
+        helloCard.style.display = "none";
+        scrollFlag = true;
+    }, TIMEOUT);
+
+}
+
+function showProjects() {
+    projectsCard.style.display = "block"; 
+    cardsContainer.style.transform = `scale(20) translate(0, 14px)`;
+
+    scrollFlag = false;
+    setTimeout(() => {
+        aboutCard.style.display = "none";
+        scrollFlag = true;
+    }, TIMEOUT);
+}
+
+// Scroll event
 addEventListener("wheel", (e) => {
     if (!scrollFlag)
         return;
@@ -58,53 +118,14 @@ addEventListener("wheel", (e) => {
         return;
     }
 
-    if (index === 2) {
-        projectsCard.style.display = "block"; 
-        cardsContainer.style.transform = `scale(20) translate(0, 14px)`;
-
-        scrollFlag = false;
-        setTimeout(() => {
-            aboutCard.style.display = "none";
-            scrollFlag = true;
-        }, 1500);
-    }
-    else if (index === 1) {
-        projectsCard.style.display = "block";
-        aboutCard.style.display = "grid";
-
-        var transform = `scale(15)`;
-        cardsContainer.style.transform = transform;
-
-        var dinoHello = helloCard.querySelector("#hello-dino");
-        dinoHello.style.transform = "scaleX(-1)";
-        dinoHello.style.left = "-1vw";
-
-        scrollFlag = false;
-        setTimeout(() => {
-            dinoHello.style.opacity = 0;
-            helloCard.style.display = "none";
-            scrollFlag = true;
-        }, 1500);
-    }
-    else {
-        projectsCard.style.display = "none";
-        helloCard.style.display = "grid";
-
-        cardsContainer.style.transform = `scale(1)`;
-        
-        var dinoHello = helloCard.querySelector("#hello-dino");
-        var houseDoor = house.querySelector(".left-side");
-        dinoHello.style.left = "0vw";
-
-        setTimeout(() => {
-            dinoHello.style.opacity = 1;
-            dinoHello.style.transform = "scaleX(1)";
-            dinoHello.style.left = "8vw";
-        }, 500)
-
-        scrollFlag = false;
-        setTimeout(() => {
-            scrollFlag = true;
-        }, 1500);
-    }
+    if (index === 2)
+        showProjects();
+    else if (index === 1)
+        showAbout();
+    else
+        showHello();
 });
+
+window.addEventListener('load', function() {
+    pageSetup();
+})
